0b0e315c80d97e257186b802bd06400facb8982b
Rabia504 <Rabia504@users.noreply.github.com>
2021-05-20 19:42:54 +0500

    monster rolodex in react

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -2,5 +2,11 @@ import React from 'react';
 import './card.styles.css';
 
 export const Card = (props) =>{
-    return <div className="card-container">{props.monster.name}</div>
+
+    return <div className="card-container">
+    <img src={`https://robohash.org/${props.monster.id}?set=set2`} width="180" height="180" alt="monster"/>
+    
+    <h2>{props.monster.name}</h2>
+    <h4>{props.monster.email}</h4>
+     </div>
 }
