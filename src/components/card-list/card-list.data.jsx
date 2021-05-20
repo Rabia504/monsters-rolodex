03357c97d9100e0dd732f774453419781e956b42
Rabia504 <Rabia504@users.noreply.github.com>
2021-05-20 16:26:44 +0500

    adding files fot github pages

---
+++ src/components/card-list/card-list.data.jsx
@@ -0,0 +1,37 @@
+const CardData = [
+    {
+     id:1,
+     spname:"Rabia Beauty Salon",
+     imgsrc:"assets/img/portfolio/p2.jpg",
+     address:"surahi Chowk,Bahria Townsurahi Chowk,Bahria Townsurahi Chowk,Bahria Town",
+     ratings:"4.5",
+     noOfratings:"200"
+    },
+    {
+        id:2,
+     spname:"Ghania Beauty Salon",
+     imgsrc:"assets/img/portfolio/img3.jpg",
+     address:"Talwar Chowk,Bahria Town",
+     ratings:"4.7",
+     noOfratings:"200"
+    },
+    {
+        id:3,
+     spname:"Aamna Beauty Salon",
+     imgsrc:"assets/img/portfolio/img7.jpg",
+     address:"Kabooter Chowk,Bahria Town",
+     ratings:"5",
+     noOfratings:"200"
+    },
+    {
+        id:4,
+     spname:"Azka Beauty Salon",
+     imgsrc:"assets/img/portfolio/img5.jpg",
+     address:"Kabooter Chowk,Bahria Town",
+     ratings:"5",
+     noOfratings:"200"
+    }
+    
+ ];
+ 
+ export default CardData;
---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './card.styles.css';
+
+export const Card = (props) =>{
+    return <div className="card-container">{props.monster.name}</div>
+}
